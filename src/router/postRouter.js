8c43c0dbbec05478f7ccedfa35ea9ff693047d03
49EHyeon42 <49ehyeon42@gmail.com>
2024-04-20 10:34:03 +0000

    fix: 에러 핸들러 분리

--- src/router/postRouter.js
+++ src/router/postRouter.js
@@ -3,6 +3,8 @@ const multer = require('multer');
 
 const PostController = require('../controller/PostController');
 
+const PostNotFoundError = require('../error/PostNotFoundError');
+
 const postController = new PostController();
 
 const router = express.Router();
@@ -15,30 +17,46 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-const errorHandler = (error, request, response, next) => {
-  let status;
-  let message;
-
-  if (error instanceof multer.MulterError) {
-    status = 400;
-    message = 'ONLY_ONE_IMAGE';
-  } else {
-    status = 500;
-    message = 'SERVER_ERROR';
-  }
-
-  response.status(status).json({ message: message });
-};
-
 router.get('/', postController.searchPost);
-router.get('/:id', postController.searchPostById);
+
+router.get(
+  '/:id',
+  postController.searchPostById,
+  (error, request, response, next) => {
+    let status;
+    let message;
+
+    if (error instanceof PostNotFoundError) {
+      status = error.statusCode;
+      message = error.message;
+    } else {
+      status = 500;
+      message = 'SERVER_ERROR';
+    }
+
+    response.status(status).json({ message: message });
+  }
+);
 
 // TODO: edit 대신 write 작성
 router.post(
   '/',
   upload.single('post-image'),
   postController.writePost,
-  errorHandler
+  (error, request, response, next) => {
+    let status;
+    let message;
+
+    if (error instanceof multer.MulterError) {
+      status = 400;
+      message = 'ONLY_ONE_IMAGE';
+    } else {
+      status = 500;
+      message = 'SERVER_ERROR';
+    }
+
+    response.status(status).json({ message: message });
+  }
 );
 
 module.exports = router;
