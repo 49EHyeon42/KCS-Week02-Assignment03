8395e0639e2f748332f1c3c59eb26cf861e673bc
49EHyeon42 <49ehyeon42@gmail.com>
2024-04-21 01:26:45 +0000

    temp: comment router 임시 구현

--- src/router/commentRouter.js
+++ src/router/commentRouter.js
@@ -1,13 +1,43 @@
 const express = require('express');
 
+const CommentController = require('../controller/CommentController');
+
+const CommentNotFoundError = require('../error/CommentNotFoundError');
+const PostNotFoundError = require('../error/PostNotFoundError');
+
+const commentController = new CommentController();
+
 const router = express.Router({ mergeParams: true });
 
-router.get('/', (request, response, next) => {
-    console.log(request.params);
+const globalCommentErrorHandler = (error, request, response, next) => {
+  let status;
+  let message;
 
-    console.log('이중 라우터 테스트: ', request.params.postId);
+  // 코드 중복, 마음에 안듬
+  if (error instanceof PostNotFoundError) {
+    status = error.status;
+    message = error.message;
+  } else if (error instanceof CommentNotFoundError) {
+    status = error.status;
+    message = error.message;
+  } else {
+    status = 500;
+    message = 'SERVER_ERROR';
+  }
 
-    response.sendStatus(200);
-});
+  response.status(status).json({ message: message });
+};
+
+router.get('/', commentController.searchComment, globalCommentErrorHandler);
+
+router.post('/', commentController.writeComment, globalCommentErrorHandler);
+
+router.patch('/:id', commentController.editComment, globalCommentErrorHandler);
+
+router.delete(
+  '/:id',
+  commentController.deleteComment,
+  globalCommentErrorHandler
+);
 
 module.exports = router;
